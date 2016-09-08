7a58a4928e0b8cca9844f6b429025f29031332b2
johannabjornholm <johanna.bjornholm@resume.se>
2016-09-08 12:10:34 +0200

    new definition of quiz

--- quiz/views.py
+++ quiz/views.py
@@ -22,10 +22,14 @@ def startpage(request):
 	context = {
 		"quizzes": quizzes,
 	}
-	return render(request, "quiz/startpage.html", context)
+	return render(request, "quiz/start.html", context)
 
-def quiz(request):
-	return render(request, "quiz/quiz.html")
+def quiz(request, quiz_number):
+	context = {
+		"quiz": quizzes[int(quiz_number) - 1],
+		"quiz_number": quiz_number,
+	}
+	return render(request, "quiz/quiz.html", context)
 
 def question(request):
 	return render(request, "quiz/question.html")
