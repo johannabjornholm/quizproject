d3c68c4096ce95dd354037e8a86aaea61d0c1b45
johannabjornholm <johanna.bjornholm@resume.se>
2016-09-08 11:58:43 +0200

    fixed return in views

--- quiz/views.py
+++ quiz/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 
 def startpage(request):
-return render(request, "quiz/start.html")
+	return render(request, "quiz/start.html")
 
 def quiz(request):
-return render(request, "quiz/quiz.html")
+	return render(request, "quiz/quiz.html")
 
 def question(request):
-return render(request, "quiz/question.html")
+	return render(request, "quiz/question.html")
 
 def completed(request):
-return render(request, "quiz/results.html")
+	return render(request, "quiz/results.html")
 
 
 # Create your views here.
