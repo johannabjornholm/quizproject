f2c6c55eceb06788e68f5f23a3b2fad6d0415e59
johannabjornholm <johanna.bjornholm@resume.se>
2016-09-08 21:15:04 +0200

    css and question changes

--- quiz/views.py
+++ quiz/views.py
@@ -3,18 +3,18 @@ from django.shortcuts import render
 quizzes = [
 	{
 		"quiz_number": 1,
-   		"name": "Klassiska böcker",
-	   	"description": "Hur bra kan du dina klassiker?"
+   		"name": "WWAHD – what would a hipster do?",
+	   	"description": "The hipster beard is a big trend these days and should stick around at least for the winter.  If you're unsure what a hipster beard is, you have seen it, I promise! Try to think about it for a minute and then try the quiz. You will rock!"
 	},
 	{
 		"quiz_number": 2,
-   	   	"name": "Största 1slagen",
-	   	"description": "Kan du dina lag?"
+   	   	"name": "Where do they go?",
+	   	"description": "So all these cool hipsters, where are they!?"
 	},
 	{
 		"quiz_number": 3,
-   	    	"name": "Världens mest kända hackare",
-	    	"description": "Hackerhistoria är viktigt, kan du den?"	},
+   	    	"name": "Hipster beards",
+	    	"description": "So, are the hipsters the ones who would only buy the cheapest drinks? Or are they the ones that are all into computers and tech? Or what was it… Well, give these questions a try and see how good you are! "	},
 ]
 
 
@@ -34,10 +34,10 @@ def quiz(request, quiz_number):
 def question(request, quiz_number, question_number):
 	context = {
 		"question_number": question_number,
-	    	"question": "Hur många bultar har ölandsbron?",
-		"answer1": "12",
-	   	"answer2": "66 400",
-	    	"answer3": "7 428 954",
+	    	"question": "Which is the most hipster city in the world?",
+		"answer1": "Stockholm, Sweden",
+	   	"answer2": "Williamsburg, Brooklyn, New York",
+	    	"answer3": "Barcelona, Spain",
 	    	"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/question.html", context)
