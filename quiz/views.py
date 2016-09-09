420d34396f93a2233004bee9ba2f6a1ed0670fa1
johannabjornholm <johanna.bjornholm@resume.se>
2016-09-09 10:52:19 +0200

    updating quiz in views

--- quiz/views.py
+++ quiz/views.py
@@ -1,28 +1,12 @@
 from django.shortcuts import render
-
-quizzes = [
-	{
-		"quiz_number": 1,
-   		"name": "WWHD – what would hipsters do?",
-	   	"description": "So, are the hipsters the ones who would only buy the cheapest drinks? Or are they the ones that are all into computers and tech? Or what was it… Well, give these questions a try and see how good you are!"
-	},
-	{
-		"quiz_number": 2,
-   	   	"name": "Where do they go?",
-	   	"description": "So all these cool hipsters, where are they!?"
-	},
-	{
-		"quiz_number": 3,
-   	    	"name": "Hipster beards",
-	    	"description": "The hipster beard is a big trend these days and should stick around at least for the winter.  If you're unsure what a hipster beard is, you have seen it, I promise! Try to think about it for a minute and then try the quiz. You will rock!"	},
-]
+from quiz.models import Quiz
 
 
 def startpage(request):
 	context = {
-		"quizzes": quizzes,
+	    	"quizzes": Quiz.objects.all(),
 	}
-	return render(request, "quiz/start.html", context)
+
 
 def quiz(request, quiz_number):
 	context = {
