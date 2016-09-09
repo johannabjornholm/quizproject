11629da194657549b0b01e945f941bf1a979ec73
johannabjornholm <johanna.bjornholm@resume.se>
2016-09-09 13:54:27 +0200

    tabs

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'quizsite.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:////{0}'.format(os.path.join(BASE_DIR,
-'db.sqlite3'))
+        'db.sqlite3'))
     )
 }
 
