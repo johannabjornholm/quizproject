17b07619c5065df6445936577f962a691fb3889a
johannabjornholm <johanna.bjornholm@resume.se>
2016-09-09 10:54:25 +0200

    adding django script for quiz

--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+
 from quiz.models import Quiz
 
 
@@ -6,7 +7,7 @@ def startpage(request):
 	context = {
 	    	"quizzes": Quiz.objects.all(),
 	}
-
+	return render(request, "quiz/start.html", context)
 
 def quiz(request, quiz_number):
 	context = {
