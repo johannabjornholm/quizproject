68427aaf644bc149b6f09f1d7c1a1b82dca56d12
johannabjornholm <johanna.bjornholm@resume.se>
2016-09-09 11:32:18 +0200

    adding question number

--- quiz/views.py
+++ quiz/views.py
@@ -6,13 +6,13 @@ from django.shortcuts import redirect
 
 def startpage(request):
 	context = {
-	    	"quizzes": Quiz.objects.all(),
+			"quizzes": Quiz.objects.all(),
 	}
 	return render(request, "quiz/start.html", context)
 
 def quiz(request, quiz_number):
 	context = {
-	    	"quiz": Quiz.objects.get(quiz_number=quiz_number),
+			"quiz": Quiz.objects.get(quiz_number=quiz_number),
 		"quiz_number": quiz_number,
 	}
 
@@ -23,31 +23,35 @@ def question(request, quiz_number, question_number):
 	questions = quiz.questions.all()
 	question = questions[int(question_number) - 1]
 	context = {
-    		"question_number": question_number,
-    		"question": question.question,
-	    	"answer1": question.answer1,
-    		"answer2": question.answer2,
-	    	"answer3": question.answer3,
-	    	"quiz": quiz,
+			"question_number": question_number,
+			"question": question.question,
+			"answer1": question.answer1,
+			"answer2": question.answer2,
+			"answer3": question.answer3,
+			"quiz": quiz,
 		"quiz_number": quiz_number,
 	}
 
 	return render(request, "quiz/question.html", context)
 
 def answer(request, quiz_number, question_number):
-    saved_answers = request.session.get(quiz_number, {})
-    answer = int(request.POST["answer"])
-    saved_answers[question_number] = answer
-    request.session[quiz_number] = saved_answers
-
-    question_number = int(question_number)
-    return redirect("question_page", quiz_number, question_number + 1)
+	saved_answers = request.session.get(quiz_number, {})
+	answer = int(request.POST["answer"])
+	saved_answers[question_number] = answer
+	request.session[quiz_number] = saved_answers
+	question_number = int(question_number)
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	num_questions = quiz.questions.count()
+	if num_questions <= question_number:
+		return redirect("completed_page", quiz_number)
+	else:
+		return redirect("question_page", quiz_number, question_number + 1)
 
 
 def completed(request, quiz_number):
 	context = {
-	    	"correct": 12,
-	    	"total": 20,
+			"correct": 12,
+			"total": 20,
 		"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/results.html", context)
